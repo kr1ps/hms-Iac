22f1dec23be82219538a8428a66b3b40a59a1f43
kr1ps <k.rodriguez.mejia@gmail.com>
2022-01-14 14:09:17 -0400

    update

--- main.tf
+++ main.tf
@@ -63,10 +63,10 @@ resource "vsphere_virtual_machine" "vm" {
     template_uuid = data.vsphere_virtual_machine.vm_template.id
   }
 
-  #this block its for auto excecute ansible playbook on new ambient. this particular one its for push ssh pub key into the user
-  #provisioner "local-exec" {
-  #  command = "ansible-playbook -u '${var.ssh_user}' -i '${vsphere_virtual_machine.vm.default_ip_address},' apache-install.yml --key-file '~/.ssh/id_ed25519'"
-  #}
+  this block its for auto excecute ansible playbook on new ambient. this particular one its for push ssh pub key into the user
+  provisioner "local-exec" {
+   command = "ansible-playbook -u '${var.ssh_user}' -i '${vsphere_virtual_machine.vm.default_ip_address},' hms.yml --key-file '~/.ssh/id_ed25519'"
+  }
 }
 
 output "vm_ip" {
