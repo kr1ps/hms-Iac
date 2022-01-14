ae75f684641d2c6ba0d06961f787787fe212afd9
kr1ps <k.rodriguez.mejia@gmail.com>
2022-01-14 19:08:04 -0400

    update

--- main.tf
+++ main.tf
@@ -65,7 +65,7 @@ resource "vsphere_virtual_machine" "vm" {
 
   # this block its for auto excecute ansible playbook on new ambient. this particular one its for push ssh pub key into the user
   # provisioner "local-exec" {
-  #  command = "ansible-playbook -u '${var.ssh_user}' -i '${vsphere_virtual_machine.vm.default_ip_address},' ./ansible-hms/hms.yml --key-file '~/.ssh/id_ed25519'"
+  #  command = "ansible-playbook -u '${var.ssh_user}' -i '${vsphere_virtual_machine.vm.default_ip_address},' ./ansible-hms/hms.yml --extra-vars 'tf_environment=${terraform.workspace}' --key-file '~/.ssh/id_ed25519'"
   # }
 }
 
